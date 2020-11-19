a299597d1efc2ddc4405cc427e5d47575cbdb421
Renan Santos Gomes <github@renansgomes.com.br>
2020-11-18 22:56:37 -0300

    Prepared environment

---
+++ README.md
@@ -0,0 +1,4 @@
+# Trabalho de teste de unidade :: The Hero Game
+
+## Grupo
+- Renan Santos Gomes
---
+++ src/test/java/MatchTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import org.testng.Assert;
+
+public class MatchTest {
+    @Test
+    public void testOneHeroAgainstAnotherUsingSpeedAtrribute(){
+        //arrange
+        int atual = 0;
+        int esperado = 0;
+
+        //act
+
+        //assert
+        Assert.assertEquals(esperado, atual);
+    }
+}
